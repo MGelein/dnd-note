699e1224350c478f984c9cd4521b757d10530557
Mees Gelein <meesgelein@gmail.com>
2018-08-24 09:18:00 +0200

    Animated markdown editor preview switch is now working

--- js/ui.js
+++ js/ui.js
@@ -1,7 +1,7 @@
 /**
  * This variable contains all UI code
  */
-const ui = {}
+var ui = {};
 /**
  * Give the variable to anonymous function once, to add all necessary functionality
  */
@@ -10,18 +10,33 @@ const ui = {}
      * Adds starting event listeners to buttons and other components
      */
     ui.addListeners = function(){
-
-    }
+        $("#notes").click(function(){
+            ui.toggleNotesView();
+        });
+    };
 
     /**
      * Possible values for the notesview
      */
     ui.notesView = {
+        CURRENT: 0,
         PREVIEW: 0,
-        SPLIT : 1,
-        EDITOR : 2
+        SPLIT_1 : 1,
+        EDITOR : 2,
+        SPLIT_2: 3,
     };
 
+    /**
+     * Sets the notesView to the next mode
+     */
+    ui.toggleNotesView = function(){
+        ui.notesView.CURRENT ++;
+        if(ui.notesView.CURRENT > ui.notesView.SPLIT_2){
+            ui.notesView.CURRENT = 0;
+        }
+        ui.setNotesView(ui.notesView.CURRENT);
+    };
+    
     /**
      * Set the notesView to one of the prepared values of
      * `ui.notesViews`
@@ -37,18 +52,22 @@ const ui = {}
         //Now see what we have to set them to
         switch(notesView){
             case ui.notesView.EDITOR:
+                console.log('set to EDITOR');
                 preview.addClass("noWidth");
                 editor.addClass("wholeWidth");
             break;
-            case ui.notesView.SPLIT:
+            case ui.notesView.SPLIT_1:
+            case ui.notesView.SPLIT_2:
+                console.log("set to SPLIT");
                 preview.addClass("halfWidth");
                 editor.addClass("halfWidth");
             break;
             default:
             case ui.notesView.PREVIEW:
+                console.log("set to PREVIEW");
                 preview.addClass("wholeWidth");
                 editor.addClass("noWidth");
             break;
         }
-    }
+    };
 })(ui);
